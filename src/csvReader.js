1aba055edf78511f7032b0ea51d33f0c5f8ccd42
dipandhali2021 <dipandhali2021@gmail.com>
2024-04-28 02:13:09 +0530

    completed till 20 updated test files and Cli support

--- src/csvReader.js
+++ src/csvReader.js
@@ -1,13 +1,30 @@
 const fs = require('fs');
 const csv = require('csv-parser');
+const { parse } = require('json2csv');
 
+// function readCSV(filePath) {
+//     const results = [];
+
+//     return new Promise((resolve, reject) => {
+//         fs.createReadStream(filePath)
+//             .pipe(csv())
+//             .on('data', (data) => results.push(data))
+//             .on('end', () => {
+//                 resolve(results);
+//             })
+//             .on('error', (error) => {
+//                 reject(error);
+//             });
+//     });
+// } 
 function readCSV(filePath) {
     const results = [];
-
     return new Promise((resolve, reject) => {
         fs.createReadStream(filePath)
             .pipe(csv())
-            .on('data', (data) => results.push(data))
+            .on('data', (data) => {
+                results.push(data)
+            })
             .on('end', () => {
                 resolve(results);
             })
@@ -17,4 +34,9 @@ function readCSV(filePath) {
     });
 }
 
-module.exports = readCSV;
+async function writeCSV(filename, data) {
+    const csv = parse(data);
+    fs.writeFileSync(filename, csv);
+}
+
+module.exports = { readCSV, writeCSV };
